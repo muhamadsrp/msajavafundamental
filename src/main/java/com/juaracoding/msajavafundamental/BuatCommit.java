61e20c424eacdcb950d12474785b1cd2f933af46
muhamadsrp <muhamadxrp@gmail.com>
2024-08-30 03:35:11 +0700

    Membuat Commit

---
+++ src/main/java/com/juaracoding/msajavafundamental/BuatCommit.java
@@ -0,0 +1,15 @@
+package com.juaracoding.msajavafundamental;
+
+
+/*
+IntelliJ IDEA 2024.2.0.2 (Community Edition)
+Build #IC-242.20224.419, built on August 19, 2024
+@Author user a.k.a. Muhamad Saripudin
+Java Developer
+Created on 30/08/2024 3:32
+@Last Modified 30/08/2024 3:32
+Version 1.0
+*/
+
+public class BuatCommit {
+}
